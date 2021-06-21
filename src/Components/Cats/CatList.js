66b5cf1c93fdcb79bf7e37f7122f036f22714bce
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-21 16:09:49 -0400

    create repo and updated cat challenge code

---
+++ src/Components/Cats/CatList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+const CatList = (props) => {
+ return (
+   <div>
+     
+     {props.cats.map((cat, index) => <li key={index}>{cat}</li> )}
+     
+   </div>
+ )
+}
+ 
+export default CatList;
